dc110ff9abea45deedb8f8638bb59aefd9c45ed3
serenaferracci <serenaferracci95@gmail.com>
2020-02-27 11:42:18 +0100

    clean up repository

--- multi_thread/Module/.fiber.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/serenaferracci/Desktop/project_aosv/aosv/multi_thread/Module/fiber.o := ld -m elf_x86_64 -z max-page-size=0x200000   -r -o /home/serenaferracci/Desktop/project_aosv/aosv/multi_thread/Module/fiber.o /home/serenaferracci/Desktop/project_aosv/aosv/multi_thread/Module/fiber_ioctl.o /home/serenaferracci/Desktop/project_aosv/aosv/multi_thread/Module/proc.o 
--- multi_thread/Module/.tmp_versions/fiber.mod
+++
@@ -1,3 +0,0 @@
-/home/serenaferracci/Desktop/project_aosv/aosv/multi_thread/Module/fiber.ko
-/home/serenaferracci/Desktop/project_aosv/aosv/multi_thread/Module/fiber_ioctl.o /home/serenaferracci/Desktop/project_aosv/aosv/multi_thread/Module/proc.o
-
